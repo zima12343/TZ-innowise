453a4eee41f9531e8b6dba893fe1227fd5ed6b53
zima12343 <zima12343@mail.ru>
2022-02-20 21:59:58 +0300

    Добавлено решение заданий

--- Task.sql
+++ Task.sql
@@ -13,7 +13,7 @@ Title		varchar(20) NOT NULL
 CREATE TABLE Clients (
 IdClient	int			PRIMARY KEY IDENTITY, 
 LName		varchar(20) NOT NULL,
-Fmane		varchar(20) NOT NULL,
+FName		varchar(20) NOT NULL,
 MName		varchar(20) NOT NULL,
 Country		varchar(30) NOT NULL, 
 City		varchar(30) NOT NULL,
@@ -50,6 +50,7 @@ CREATE TABLE Accounts (
 	BankId			int NOT NULL,
 	ClientId		int NOT NULL,
 	AccountNumber	varchar(50) NOT NULL UNIQUE,
+	Balance		numeric(8,2) DEFAULT 0, 
 	UNIQUE(BankId, ClientId),  --1 клиент - 1 счет в банке
 	UNIQUE(BankId, AccountNumber),  --В каждом банке уникальный счет
 	CONSTRAINT [FK_Accounts_To_Bank] FOREIGN KEY (BankId) REFERENCES [Banks](IdBank),
@@ -61,6 +62,7 @@ CardNumber	numeric(16,0) PRIMARY KEY,
 ValidDate	Date NOT NULL,
 CVC			numeric(3,0), 
 AccountId	int NOT NULL,
+Balance		numeric(8,2) DEFAULT 0
 CONSTRAINT [FK_Cards_To_Account] FOREIGN KEY (AccountId) REFERENCES [Accounts](IdAccount)
 )
 
@@ -74,7 +76,7 @@ INSERT INTO [SocialStatuses]
 
 INSERT INTO [dbo].[Clients]
            ([LName]
-           ,[Fmane]
+           ,[FName]
            ,[MName]
            ,[Country]
            ,[City]
@@ -128,34 +130,171 @@ INSERT INTO [BankBranches]
 INSERT INTO [Accounts]
            ([BankId]
            ,[ClientId]
-           ,[AccountNumber])
+           ,[AccountNumber]
+		   ,[Balance])
      VALUES
-			(1,2,'Xjq04iu4KI5u'),
-			(1,1,'Pe5Ts5ef4J8L'),
-			(2,1,'cWfv4OXmYx0K'),
-			(2,3,'sBTB37rU3TBB'),
-			(2,2,'lnksjdLOS4pR'),
-			(3,1,'RvQoPGKr9CRX'),
-			(3,5,'S5QHIN9vyb3P'),
-			(5,4,'NgFwmLgZCsbl'),
-			(3,3,'j2yoCdUgJfrX'),
-			(4,3,'Q67UTuxVkTcD')
+			(1,2,'Xjq04iu4KI5u', (SELECT ROUND(RAND() * 1000, 2))),
+			(1,1,'Pe5Ts5ef4J8L', (SELECT ROUND(RAND() * 1000, 2))),
+			(2,1,'cWfv4OXmYx0K', (SELECT ROUND(RAND() * 1000, 2))),
+			(2,3,'sBTB37rU3TBB', (SELECT ROUND(RAND() * 1000, 2))),
+			(2,2,'lnksjdLOS4pR', (SELECT ROUND(RAND() * 1000, 2))),
+			(3,1,'RvQoPGKr9CRX', (SELECT ROUND(RAND() * 1000, 2))),
+			(3,5,'S5QHIN9vyb3P', (SELECT ROUND(RAND() * 1000, 2))),
+			(5,4,'NgFwmLgZCsbl', (SELECT ROUND(RAND() * 1000, 2))),
+			(3,3,'j2yoCdUgJfrX', (SELECT ROUND(RAND() * 1000, 2))),
+			(4,3,'Q67UTuxVkTcD', (SELECT ROUND(RAND() * 1000, 2)))
 
 
 INSERT INTO [Cards]
            ([CardNumber]
            ,[ValidDate]
            ,[CVC]
-           ,[AccountId])
+           ,[AccountId]
+		   ,[Balance])
      VALUES
-            (5404366485148734, '01.11.2025', 631, 1),
-		    (5404365883659243, '01.12.2022', 290, 2),
-			(5404365053557763, '01.01.2023', 725, 3),
-			(5404367836469803, '01.02.2024', 137, 4),
-			(5404361197558981, '01.06.2022', 807, 3),
-			(5404362699176645, '01.12.2023', 888, 5),
-			(5404365334689831, '01.10.2024', 435, 6),
-			(5404367292029547, '01.5.2025', 544, 7),
-			(5404369161050729, '01.7.2022', 904, 8)
+            (5404366485148734, '01.11.2025', 631, 1, (SELECT ROUND(RAND() * 1000, 2))),
+		    (5404365883659243, '01.12.2022', 290, 2, (SELECT ROUND(RAND() * 1000, 2))),
+			(5404365053557763, '01.01.2023', 725, 3, (SELECT ROUND(RAND() * 1000, 2))),
+			(5404367836469803, '01.02.2024', 137, 4, (SELECT ROUND(RAND() * 1000, 2))),
+			(5404361197558981, '01.06.2022', 807, 3, (SELECT ROUND(RAND() * 1000, 2))),
+			(5404362699176645, '01.12.2023', 888, 5, (SELECT ROUND(RAND() * 1000, 2))),
+			(5404365334689831, '01.10.2024', 435, 6, (SELECT ROUND(RAND() * 1000, 2))),
+			(5404367292029547, '01.05.2025', 544, 7, (SELECT ROUND(RAND() * 1000, 2))),
+			(5404369161050729, '01.07.2022', 904, 8, (SELECT ROUND(RAND() * 1000, 2)))
 
 
+UPDATE A 
+SET Balance = (SELECT SUM(Balance) 
+			   FROM Cards 
+			   WHERE a.IdAccount = AccountId)
+FROM  Cards c
+	INNER JOIN Accounts a ON a.IdAccount = c.AccountId
+WHERE (a.Balance - (SELECT SUM(Balance) 
+					FROM Cards 
+					WHERE a.IdAccount = AccountId)) < 0
+	
+
+			--Задание 1
+SELECT b.* 
+FROM Banks b 
+	INNER JOIN BankBranches bb ON bb.BankId = b.IdBank
+WHERE BB.City = 'Москва'
+
+		--Задание 2
+SELECT b.Title Bank, 
+		LName, 
+		FName, 
+		MName, 
+		c.CardNumber, 
+		a.AccountNumber 
+FROM Cards c
+	INNER JOIN Accounts a ON a.IdAccount = c.AccountId
+	INNER JOIN Clients cl ON cl.IdClient = a.ClientId
+	INNER JOIN Banks b ON b.IdBank = a.BankId
+
+
+			--Задание 3 (C помощью подзапроса)
+SELECT DISTINCT a.*, 
+				ABS(a.Balance - (SELECT SUM(Balance) 
+								 FROM Cards 
+								 WHERE a.IdAccount = AccountId))
+FROM  Cards c
+	INNER JOIN Accounts a ON a.IdAccount = c.AccountId
+WHERE a.Balance <> (SELECT SUM(Balance) 
+					FROM Cards 
+					WHERE a.IdAccount = AccountId)
+
+
+					--Задание 4.1
+SELECT ss.Title, 
+	   COUNT(ss.Title) [COUNT]
+FROM Clients c
+	INNER JOIN SocialStatuses ss ON ss.IdStatus = c.StatusId
+GROUP BY ss.Title
+
+					--Задание 4.2
+
+SELECT Title, 
+	   (SELECT COUNT(*) 
+	    FROM Clients 
+	    WHERE StatusId = IdStatus) [COUNT]
+FROM  SocialStatuses;
+
+
+					--Задание 5
+
+CREATE PROCEDURE [sp_AddForStatus]
+	@StatusId int,
+	@Sum numeric(8,2),
+	@RetVal varchar(30) OUTPUT
+AS
+IF @Sum < 0 
+BEGIN
+	SET @RetVal = 'Сумма должна быть > 0'
+	return 0
+END
+	UPDATE a 
+	SET Balance = Balance + @Sum 
+	FROM Accounts a 
+		INNER JOIN Clients c ON c.IdClient = a.ClientId
+	WHERE StatusId = @StatusId
+	SET @RetVal = 'Выполнено успешно'
+RETURN 0
+
+DECLARE @rv varchar(30)
+exec sp_AddForStatus 1, 10, @rv OUTPUT
+SELECT @rv
+
+		-- Задание 6  С помощью оконной ф-ции 
+
+SELECT DISTINCT LName, 
+				FName,
+				MName,
+				a.AccountNumber,
+				(a.Balance - SUM(c.Balance) over(PARTITION BY a.IdAccount)) FreeMoney
+FROM  Cards c
+	INNER JOIN Accounts a ON a.IdAccount = c.AccountId
+	INNER JOIN Clients cl on cl.IdClient = a.ClientId
+
+
+
+			-- Задание 7
+
+CREATE PROCEDURE [sp_TransferMoney]
+	@AccountId int,
+	@CardNum numeric(16,0),
+	@Sum numeric(8,2),
+	@RetVal varchar(30) OUTPUT
+AS
+	BEGIN TRANSACTION tr1
+	if (SELECT Count(*) 
+		FROM Cards 
+		WHERE AccountId = @AccountId 
+			AND CardNumber = @CardNum) <> 1 
+	BEGIN
+		SET @RetVal = 'Карта не пренадлежит счёту'
+		ROLLBACK TRANSACTION tr1
+		RETURN 0
+	END
+	UPDATE Cards 
+	SET Balance = Balance + @Sum
+	WHERE CardNumber = @CardNum
+
+	if (SELECT a.Balance - (SUM(c.Balance) over(PARTITION BY a.IdAccount))
+		FROM Cards c
+			INNER JOIN Accounts a ON a.IdAccount = AccountId
+		WHERE AccountId = @AccountId)  < 0 
+	BEGIN
+		SET @RetVal = 'Перевод выполнить невозможно.'
+		ROLLBACK TRANSACTION tr1
+		RETURN 0
+	END
+	SET @RetVal = 'Перевод выполнен успешно'
+	COMMIT TRANSACTION TR1
+RETURN 0
+
+
+
+DECLARE @rv varchar(30)
+exec sp_TransferMoney 2, 5404365883659243, 10, @rv OUTPUT
+SELECT @rv
